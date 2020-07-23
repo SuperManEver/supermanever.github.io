af8db8119bda4c135ecff59066ab2651e179e34f
SuperManEver <nikita.luparev@gmail.com>
2020-07-23 17:15:07 +0500

    change credentials

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,15 +1,19 @@
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
+importScripts(
+  "https://cdnjs.cloudflare.com/ajax/libs/firebase/7.16.1/firebase-app.min.js"
+);
+importScripts(
+  "https://cdnjs.cloudflare.com/ajax/libs/firebase/7.16.1/firebase-messaging.min.js"
+);
 
 firebase.initializeApp({
-  messagingSenderId: '448358493027'
+  messagingSenderId: "384403560705",
 });
 
 const messaging = firebase.messaging();
 
 // Customize notification handler
-messaging.setBackgroundMessageHandler(function(payload) {
-  console.log('Handling background message', payload);
+messaging.setBackgroundMessageHandler(function (payload) {
+  console.log("Handling background message", payload);
 
   // Copy data object to get parameters in the click handler
   payload.data.data = JSON.parse(JSON.stringify(payload.data));
@@ -17,23 +21,27 @@ messaging.setBackgroundMessageHandler(function(payload) {
   return self.registration.showNotification(payload.data.title, payload.data);
 });
 
-self.addEventListener('notificationclick', function(event) {
-  const target = event.notification.data.click_action || '/';
+self.addEventListener("notificationclick", function (event) {
+  const target = event.notification.data.click_action || "/";
   event.notification.close();
 
   // This looks to see if the current is already open and focuses if it is
-  event.waitUntil(clients.matchAll({
-    type: 'window',
-    includeUncontrolled: true
-  }).then(function(clientList) {
-    // clientList always is empty?!
-    for (var i = 0; i < clientList.length; i++) {
-      var client = clientList[i];
-      if (client.url === target && 'focus' in client) {
-        return client.focus();
-      }
-    }
+  event.waitUntil(
+    clients
+      .matchAll({
+        type: "window",
+        includeUncontrolled: true,
+      })
+      .then(function (clientList) {
+        // clientList always is empty?!
+        for (var i = 0; i < clientList.length; i++) {
+          var client = clientList[i];
+          if (client.url === target && "focus" in client) {
+            return client.focus();
+          }
+        }
 
-    return clients.openWindow(target);
-  }));
+        return clients.openWindow(target);
+      })
+  );
 });
